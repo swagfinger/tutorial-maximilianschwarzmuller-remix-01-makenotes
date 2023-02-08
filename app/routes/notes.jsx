5983968e8f836bf72a072ff256d32a08380ec659
swagfinger <swagfinger@gmail.com>
2023-02-08 17:27:47 +0800

    linking styling via external files - surfacing component styles (global styling)

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,7 +1,13 @@
+import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+
 export default function NotesPage() {
   return (
     <main>
-      <h1>my notes</h1>
+      <NewNote />
     </main>
   );
+}
+
+export function links() {
+  return [...newNoteLinks()];
 }
