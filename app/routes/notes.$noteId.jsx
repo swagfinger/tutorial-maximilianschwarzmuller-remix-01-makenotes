cf3dad55bc1ccf6485224a5869ea852e5bf2fee2
swagfinger <swagfinger@gmail.com>
2023-02-09 11:23:44 +0800

    fetching dynamic route data

---
+++ app/routes/notes.$noteId.jsx
@@ -0,0 +1,40 @@
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+import styles from '~/styles/note-details.css';
+import { getStoredNotes } from '../data/notes';
+
+export default function NoteDetailsPage() {
+  const note = useLoaderData();
+
+  return (
+    <main id='note-details'>
+      <header>
+        <nav>
+          <Link to='/notes'>Back to all notes</Link>
+        </nav>
+        <h1>{note.title}</h1>
+      </header>
+      <p id='note-details-content'>{note.content}</p>
+    </main>
+  );
+}
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
+
+// fetching data for dynamic routes -
+//receives a data object passed in by remix automatically
+//(with a 'request' property that has details get request and
+//'params' property about route path
+//in routes/ we have notes.$noteId.jsx
+
+export async function loader({ params }) {
+  const notes = await getStoredNotes();
+  const noteId = params.noteId;
+  const selectedNote = notes.find((node) => node.id === noteId);
+  if (!selectedNote) {
+    throw json({ message: 'could not find note for id' + noteId });
+  }
+  return selectedNote;
+}
